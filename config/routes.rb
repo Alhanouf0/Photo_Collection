c715bf4f4338e7a786d1313183d8cbdbfb83e054
Alhanouf0 <aalmansour@mrsool.co>
2022-03-12 22:59:38 +0300

    API features commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :venues
+  resources :users do
+    resources :pictures
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
